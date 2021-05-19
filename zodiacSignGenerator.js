6ebf005e16279b6778e79cd4d1aada9042c317c3
mmoyer94 <84200283+mmoyer94@users.noreply.github.com>
2021-05-19 17:00:53 -0400

    Initial Commit

---
+++ zodiacSignGenerator.js
@@ -0,0 +1,15 @@
+
+
+const getZodiacSign = (birthMonth, birthDate) => {
+
+    if (birthMonth = 'January' && birthDate > 18 && birthDate < 31) {
+        return "Aquarius";
+
+    } else {return "error!"};
+
+
+
+    }
+
+  
+}
