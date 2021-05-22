332f42ce52c5ece6512d2417e426bdc806974c26
mmoyer94 <84200283+mmoyer94@users.noreply.github.com>
2021-05-22 06:50:32 -0400

    added getRandome Fortune Function

--- horoscopeGenerator.js
+++ horoscopeGenerator.js
@@ -1,58 +1,56 @@
-const getHoroscope = (birthMonth, birthDate) => {
-
-    return {
-        
-        zodiacSign: zodiacSign,
-        
-        currentFortune: currentFortune,
-        
-        futureOutlook: futureOutlook,
-    
-
-
 const getZodiacSign = (birthMonth, birthDate) => {
 
-    if (birthMonth == 'January' && birthDate >= 20 && birthDate <= 31 || birthMonth == 'February' && birthDate > 0 && birthDate <= 18) {
-        "Aquarius";
-        return this.zodiacsign;
+    if (birthMonth = 'January' && birthDate >= 20 && birthDate <= 31 || birthMonth == 'February' && birthDate > 0 && birthDate <= 18) {
+        return "Zodiac Sign: Aquarius";
     }
-    else if (birthMonth == 'February' && birthDate >= 19 && birthDate <= 29 || birthMonth == 'March' && birthDate > 0 && birthDate < 20) {
-        return "Pisces";
+     else if (birthMonth = 'February' && birthDate >= 19 && birthDate <= 29 || birthMonth == 'March' && birthDate > 0 && birthDate < 20) {
+        return "Zodiac Sign: Pisces";
     } 
-    else if (birthMonth == 'March' && birthDate >= 21 && birthDate < 31 || birthMonth == 'April' && birthDate > 0 && birthDate <= 19) {
-        return "Aries";
+    else if (birthMonth = 'March' && birthDate >= 21 && birthDate < 31 || birthMonth == 'April' && birthDate > 0 && birthDate <= 19) {
+        return "Zodiac Sign: Aries";
     }
-    else if (birthMonth == 'April' && birthDate >= 20 && birthDate <= 30 || birthMonth == 'May' && birthDate > 0 && birthDate <= 20) {
-        return "Taurus";
+    else if (birthMonth = 'April' && birthDate >= 20 && birthDate <= 30 || birthMonth == 'May' && birthDate > 0 && birthDate <= 20) {
+        return "Zodiac Sign: Taurus";
     }
-    else if (birthMonth == 'May' && birthDate >= 21 && birthDate <= 31 || birthMonth == 'June' && birthDate > 0 && birthDate <= 20) {
-        return  "Gemini";
+    else if (birthMonth = 'May' && birthDate >= 21 && birthDate <= 31 || birthMonth == 'June' && birthDate > 0 && birthDate <= 20) {
+        return  "Zodiac Sign: Gemini";
     }
-    else if (birthMonth == 'June' && birthDate >= 21 && birthDate <= 30 || birthMonth == 'July' && birthDate > 0 && birthDate <= 22) {
-        return "Cancer";
+    else if (birthMonth = 'June' && birthDate >= 21 && birthDate <= 30 || birthMonth == 'July' && birthDate > 0 && birthDate <= 22) {
+        return "Zodiac Sign: Cancer";
     }
-    else if (birthMonth == 'July' && birthDate >= 23 && birthDate <= 31 || birthMonth == 'August' && birthDate > 0 && birthDate <= 22) {
-        return "Leo";
+    else if (birthMonth = 'July' && birthDate >= 23 && birthDate <= 31 || birthMonth == 'August' && birthDate > 0 && birthDate <= 22) {
+        return "Zodiac Sign: Leo";
     }
-    else if (birthMonth == 'August' && birthDate >= 23 && birthDate <= 31 || birthMonth == 'September' && birthDate > 0 && birthDate <= 22) {
-        return "Virgo";
+    else if (birthMonth = 'August' && birthDate >= 23 && birthDate <= 31 || birthMonth == 'September' && birthDate > 0 && birthDate <= 22) {
+        return "Zodiac Sign: Virgo";
     }
-    else if (birthMonth == 'September' && birthDate >= 23 && birthDate <= 30 || birthMonth == 'October' && birthDate > 0 && birthDate <= 22) {
-        return "Libra";
+    else if (birthMonth = 'September' && birthDate >= 23 && birthDate <= 30 || birthMonth == 'October' && birthDate > 0 && birthDate <= 22) {
+        return "Zodiac Sign: Libra";
     }
-    else if (birthMonth == 'October' && birthDate >= 23 && birthDate <= 31 || birthMonth == 'November' && birthDate > 0 && birthDate <= 21) {
-        return "Scorpio";
+    else if (birthMonth = 'October' && birthDate >= 23 && birthDate <= 31 || birthMonth == 'November' && birthDate > 0 && birthDate <= 21) {
+        return "Zodiac Sign: Scorpio";
     }
-    else if (birthMonth == 'November' && birthDate >= 22 && birthDate <=30 || birthMonth == 'December' && birthDate > 0 && birthDate <= 21) {
-        return "Sagittarius";
+    else if (birthMonth = 'November' && birthDate >= 22 && birthDate <=30 || birthMonth == 'December' && birthDate > 0 && birthDate <= 21) {
+        return "Zodiac Sign: Sagittarius";
     }
-    else if (birthMonth == 'December' && birthDate >= 22 && birthDate <= 31 || birthMonth == 'January' && birthDate > 0 && birthDate <= 19) {
+    else if (birthMonth = 'December' && birthDate >= 22 && birthDate <= 31 || birthMonth == 'January' && birthDate > 0 && birthDate <= 19) {
         return "Capricorn";
-    } else {return "Error!"};
+    } else {return "Error! Invalid Birthday"};
 
     }
-  }
-} 
+  
+const getRandomFortune() {
+    currentOutlookMessageArray = ['You are having good luck','You are going through a tough time', 'You are in limbo', 'You feel restless','You are hopeful','Your sense of hope is waning','You feel emotionally exhausted','You are excited about an upcoming opportunity'];
+    futureOutlookMessageArray = ['There are happier times ahead, keep pushing forward', 'There is trouble ahead, beware', 'You will be tested, but you will overcome', 'If you do not correct course, you will suffer for it in the future', 'Your future goal is attainable, keep working', 'Life will soon get tough, but once the storm has passed, clear skies ahead' ];
 
-console.log(getZodiacSign('January', 21));
+    return {
+        currentOutlookMessageArray[i * Math.floor(Math.random() * 8)];
+        futureOutlookMeesageArray[i * Math.floor(Math.random() * 7)]
+    }
+}
+ 
 
+let testHoroscope = getZodiacSign('January', 23);
+
+console.log(testHoroscope);
+console.log(getRandomFortune);
